9deac1d975709bc347e08fffa276fd6782eb578f
Clément Rigo <rigoclement@mydnic.be>
2021-07-09 14:09:53 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ['@nuxtjs'],
+    rules: {
+        indent: ['error', 4],
+        'vue/script-indent': ['error', 4, { baseIndent: 0 }],
+        'vue/html-indent': ['error', 4, { baseIndent: 1 }]
+    }
+}
---
+++ src/renderer/pages/login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="e-nuxt-container">
+    <div class="e-nuxt-content">
+      <button @click="setWallpaper">download</button>
+    </div>
+  </div>
+</template>
+
+<script>
+const wallpaper = require('wallpaper')
+const download = require('download')
+export default {
+  data: () => ({
+    valid: false,
+    firstname: '',
+    lastname: '',
+    nameRules: [
+      v => !!v || 'Name is required',
+      v => v.length <= 10 || 'Name must be less than 10 characters'
+    ],
+    email: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+/.test(v) || 'E-mail must be valid'
+    ]
+  }),
+
+  methods: {
+    async setWallpaper () {
+      await download('https://9wallpapers.com/cdn/wallpapers/XRmddQQeRqE4bmQ5rHWZOBs6gBOFoNPHH30PRPnJ.jpg', 'dist')
+      await wallpaper.set('dist/XRmddQQeRqE4bmQ5rHWZOBs6gBOFoNPHH30PRPnJ.jpg')
+    }
+  }
+}
+</script>
