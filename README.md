fdb8a908dab841b643489e1ea991aa30a225b7b1
Clément Rigo <rigoclement@mydnic.be>
2022-06-02 14:04:45 +0200

    fix build

--- README.md
+++ README.md
@@ -18,4 +18,6 @@ yarn build
 
 ---
 
-This project was generated with [electron-nuxt](https://github.com/michalzaq12/electron-nuxt) v1.8.1 using [vue-cli](https://github.com/vuejs/vue-cli). Documentation about the original structure can be found [here](https://github.com/michalzaq12/electron-nuxt/blob/master/README.md).
+NEED Python2 to run
+
+export PYTHON_PATH="/usr/local/bin/python"
--- builder.config.js
+++ builder.config.js
@@ -1,72 +1,76 @@
 const ICONS_DIR = 'build/icons/'
 
 const windowsOS = {
-  win: {
-    icon: ICONS_DIR + 'win-icon.ico',
-    publisherName: 'michal',
-    target: 'nsis'
-  },
+    win: {
+        icon: ICONS_DIR + 'win-icon.ico',
+        publisherName: '9wallpapers.com',
+        target: 'nsis'
+    },
 
-  nsis: {
-    differentialPackage: true
-  }
+    nsis: {
+        differentialPackage: true
+    }
 }
 
 const linuxOS = {
-  linux: {
-    icon: ICONS_DIR,
-    target: 'deb'
-  }
+    linux: {
+        icon: ICONS_DIR,
+        target: 'deb'
+    }
 }
 
 const macOS = {
-  mac: {
-    target: 'dmg',
-    icon: ICONS_DIR + 'con.icns'
-  },
-  dmg: {
-    contents: [
-      {
-        x: 410,
-        y: 150,
-        type: 'link',
-        path: '/Applications'
-      },
-      {
-        x: 130,
-        y: 150,
-        type: 'file'
-      }
-    ]
-  }
+    mac: {
+        target: 'dmg',
+        icon: ICONS_DIR + 'con.icns'
+    },
+    dmg: {
+        contents: [
+            {
+                x: 410,
+                y: 150,
+                type: 'link',
+                path: '/Applications'
+            },
+            {
+                x: 130,
+                y: 150,
+                type: 'file'
+            }
+        ]
+    }
 }
 
 module.exports = {
-  productName: '9wallpapers-desktop',
-  appId: 'com.9wallpapers.desktop',
-  artifactName: 'setup-${version}.${ext}',
-  directories: {
-    output: 'build'
-  },
-  // default files: https://www.electron.build/configuration/contents
-  files: [
-    'package.json',
-    {
-      from: 'dist/main/',
-      to: 'dist/main/'
+    productName: '9wallpapers-desktop',
+    appId: 'com.9wallpapers.desktop',
+    artifactName: '9wallpapers-app-${version}.${ext}',
+    directories: {
+        output: 'build'
     },
-    {
-      from: 'dist/renderer',
-      to: 'dist/renderer/'
-    }
-  ],
-  extraResources: [
-    {
-      from: 'src/extraResources/',
-      to: ''
-    }
-  ],
-  ...windowsOS,
-  ...linuxOS,
-  ...macOS
+    // default files: https://www.electron.build/configuration/contents
+    files: [
+        'package.json',
+        {
+            from: 'dist/main/',
+            to: 'dist/main/'
+        },
+        {
+            from: 'dist/renderer',
+            to: 'dist/renderer/'
+        }
+    ],
+    extraResources: [
+        {
+            from: 'src/extraResources/',
+            to: ''
+        },
+        {
+            from: 'node_modules/wallpaper/source/',
+            to: 'app.asar.unpacked/dist/main'
+        }
+    ],
+    ...windowsOS,
+    ...linuxOS,
+    ...macOS
 }
